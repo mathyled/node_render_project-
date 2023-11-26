9808f6284eecb7e0f790ce0a43e7398ce7dc73ac
mathyled <mathiasimagine@gmail.com>
2023-11-26 20:55:04 -0300

    refactor

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import pg from "pg";
+import { config } from "dotenv";
+config()
+const { DB_URL } = process.env;
+
+const pool = new pg.Pool({
+    connectionString: DB_URL,
+    // ssl:true
+})
 
 const app = express()
 const PORT = 3000
@@ -6,6 +15,11 @@ const PORT = 3000
 
 app.get("/", (req,res) => {
   res.send("holaaa")
+})
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query('SELECT NOW()')
+  res.json(result.rows[0])
 })
 
 app.listen(PORT, () => {
